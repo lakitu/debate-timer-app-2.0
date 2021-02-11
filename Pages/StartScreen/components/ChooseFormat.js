574e8310bf49f6eb5b4bed564f8373e96e243c89
Lakitu <krishin901@gmail.com>
2021-02-11 15:29:52 -0500

    Changed the dropdown package because the original one does not work with managed expo apps.

--- Pages/StartScreen/components/ChooseFormat.js
+++ Pages/StartScreen/components/ChooseFormat.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Picker} from '@react-native-picker/picker'
+import {Picker} from 'react-native'
 import {startStyles} from "../styles";
 
 export class ChooseFormat extends React.Component {
@@ -16,7 +16,7 @@ export class ChooseFormat extends React.Component {
             this.setState({
                 selectedFormat: this.props.formats[0],
                 formats: this.props.formats,
-            })
+            });
         } catch (e) {
             this.setState({
                 selectedFormat: ["loading", "load"],
@@ -34,9 +34,7 @@ export class ChooseFormat extends React.Component {
     }
 
     render() {
-        if (this.state.selectedFormat == null) {
-            return null;
-        } else {
+        if (this.state.selectedFormat !== null) {
             return (
                 <Picker //TODO: make the dropdown change on ios
                     selectedValue={this.state.selectedFormat[1]}
@@ -44,10 +42,12 @@ export class ChooseFormat extends React.Component {
                     onValueChange={this.changeSelected}
                 >
                     {this.props.formats.map((format, i) => {
+                        console.log(format, i);
                         return (<Picker.Item label={format[0]} value={format[1]} key={i}/>)
                     })}
                 </Picker>
             )
         }
+        return null;
     }
 }
